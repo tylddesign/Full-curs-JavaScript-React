d387e1261eaf1cac357457b0a1a230d3c6e75961
Alexey Goryainov <vladimirovich.alex.gor@gmail.com>
2023-09-20 08:57:37 +0200

    26 урок

--- 06 - Библиотека React. Базовый уровень/004 Create React App - создаем свое приложение/my-app/src/index.js
+++ 06 - Библиотека React. Базовый уровень/004 Create React App - создаем свое приложение/my-app/src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
+import styled from 'styled-components';
+
+import { Button } from '../src/components/app/app.js'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+`
+
 root.render(
   <React.StrictMode>
     <App />
+    <BigButton as="a">+++</BigButton>
   </React.StrictMode>
 );
 
